27cf2f6c4a65f1287fd0404b3d6d9c4f1633d0f5
David Roetzel <david@roetzel.de>
2023-05-26 11:57:47 +0200

    Query key data w/o environment.
    Hosts might not have configured their environment in
    foreman (in fact the whole environment might be missing from
    foreman) or it might be incorrect.
    
    This way, HDM will chose the correct environment for the given
    host.

--- app/services/proxy_api/hdm.rb
+++ app/services/proxy_api/hdm.rb
@@ -6,21 +6,11 @@ module ProxyAPI
     end
 
     def keys(host)
-      hostname, environment = get_hostname_and_environment(host)
-
-      get("/environments/#{environment}/nodes/#{hostname}/keys")
+      get("/nodes/#{host.fqdn}/keys")
     end
 
     def key(host, key)
-      hostname, environment = get_hostname_and_environment(host)
-
-      get("/environments/#{environment}/nodes/#{hostname}/keys/#{key}")
-    end
-
-    private
-
-    def get_hostname_and_environment(host)
-      [host.fqdn, host.environment.name]
+      get("/nodes/#{host.fqdn}/keys/#{key}")
     end
   end
 end
